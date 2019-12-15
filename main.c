95867a565925c1c8016b41043820579f65ed6218
Abh15h3k <abhishekbanerji1999@gmail.com>
2019-12-15 22:54:16 +0530

    just a simple program to make sure all basic operations are working properly

---
+++ main.c
@@ -0,0 +1,36 @@
+#include "bigint.h"
+#include <stdio.h>
+
+int main()
+{
+    bigint *b1 = bigint_init(), *b2 = bigint_init();
+    bigint *result = bigint_init();
+    bigint *result1 = bigint_init();
+
+    bigint_fromstring(b1, "1234567890");
+    bigint_fromstring(b2, "1000");
+
+    bigint_add(result, b1, b2);
+    bigint_print(result);
+
+    bigint_subtract(result, b1, b2);
+    bigint_print(result);
+
+    bigint_multiply(result, b1, b2);
+    bigint_print(result);
+
+    bigint_divide(result, result1, b1, b2);
+    bigint_print(result);
+    bigint_print(result1);
+
+    bigint_remainder(b1, b2, result1);
+    bigint_print(result1);
+
+    bigint_deinit(result);
+    bigint_deinit(result1);
+    bigint_deinit(b1);
+    bigint_deinit(b2);
+
+
+    return 0;
+}
